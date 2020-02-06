c7b27d21c44ae2891075c78d19f698fd23b97c26
ChrisHeiden <christopherheiden9@gmail.com>
2020-02-06 12:14:16 +0100

    fix flashing of the microcontroller, so data will print out

--- InteractionSender/InteractionSender.ino
+++ InteractionSender/InteractionSender.ino
@@ -1,135 +1,180 @@
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
 
-/**********Distant Sensor**************/
+/********Distance Sensor********/
 #include <VL53L1X.h>
 
-/**********Acceleration**************/
+/********Accel Sensor********/
+#include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
 
-/**********Gyroscope**************/
+/********Orientation Sensor********/
 #include <Adafruit_L3GD20_U.h>
 #include <Adafruit_9DOF.h>
 
-VL53L1X sensor;
-Adafruit_9DOF dof = Adafruit_9DOF();
+/********RFID chip********/
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN D8
+#define RST_PIN D3
+ 
+
+VL53L1X distanceSensor;
+
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
-Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(30302);
+
+Adafruit_9DOF dof   = Adafruit_9DOF();
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key; 
+byte nuidPICC[4];
 
 void setup()
 {
   Serial.begin(115200);
-
-  /**********Distant Sensor**************/
   Wire.begin();
-  Wire.setClock(400000);
+  Wire.setClock(400000); // use 400 kHz I2C
 
-  sensor.setTimeout(500);
-  if (!sensor.init())
+  /********Distance Sensor********/
+  distanceSensor.setTimeout(500);
+  if (!distanceSensor.init())
   {
+    Serial.println("Failed to detect and initialize sensor!");
     while (1);
   }
   
-  sensor.setDistanceMode(VL53L1X::Long);
-  sensor.setMeasurementTimingBudget(50000);
-  sensor.startContinuous(50);
+  distanceSensor.setDistanceMode(VL53L1X::Long);
+  distanceSensor.setMeasurementTimingBudget(50000);
+  distanceSensor.startContinuous(50);
 
-  /**********Accelerometer**************/
+  /********Accel Sensor********/
   #ifndef ESP8266
-    while (!Serial)
-      ;
+    while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
   #endif
+    Serial.begin(115200);
+    Serial.println("Accelerometer Test"); Serial.println("");
 
-  if (!accel.begin()) {
-    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-    while (1)
-      ;
+    /* Initialise the sensor */
+    if(!accel.begin())
+    {
+      /* There was a problem detecting the ADXL345 ... check your connections */
+      Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+      while(1);
+    }
+
+  /********Orientation Sensor********/
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
   }
-  
   if(!mag.begin())
   {
     /* There was a problem detecting the LSM303 ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while(1);
   }
-  
+
+  /********RFID chip********/
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 
+
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
+  Serial.print(F("Using the following key:"));
 }
 
 void loop()
 {
+  /********Distance Sensor********/
+  Serial.print(distanceSensor.read()); // in mm
+  if (distanceSensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+  Serial.println();
+
+  /********Accel Sensor********/
   sensors_event_t accel_event;
-  float gyroscopeData[3]; 
-  float accelerationData[3]; 
+  accel.getEvent(&accel_event);
 
-  //if I just use a pointer -> rebooting loop
-  int distance = getDistance();
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(accel_event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(accel_event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(accel_event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
 
-  //Serial.println(distance);
-  float* gyroscopeDataPointer = getGyroscopeData(accel_event, gyroscopeData);
-  /*
-  Serial.print("roll: ");
-  Serial.print(gyroscopeDataPointer[0]);
-  Serial.print("  ");
-  Serial.print("pitch: ");
-  Serial.print(gyroscopeDataPointer[1]);
-  Serial.print("  ");
-  Serial.print("heading: ");
-  Serial.print(gyroscopeDataPointer[2]);
-  Serial.println("  ");
-  */
-  float* accelerationDataPointer = getAccelerationData(accel_event, accelerationData);
-  /*
-  Serial.print("X: ");
-  Serial.print(accelerationDataPointer[0]);
-  Serial.print("  ");
-  Serial.print("Y: ");
-  Serial.print(accelerationDataPointer[1]);
-  Serial.print("  ");
-  Serial.print("Z: ");
-  Serial.print(accelerationDataPointer[2]);
-  Serial.print("  ");
-  Serial.println("m/s^2");
-  */
-}
-
-/**
- * get distance sensor value  
- * @return {int} - distance
- */
-int getDistance(){
-  int distance = sensor.read();
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-  return distance;
-}
-
-/**
- * get gyroscope sensor values  
- * @return {float*} - gyroscope array values [roll,pitch,heading]
- */
-float* getGyroscopeData(sensors_event_t accel_event, float *gyroscopeData){
+  /********Orientation Sensor********/
   sensors_event_t mag_event;
   sensors_vec_t orientation;
-  
-  accel.getEvent(&accel_event);
-  mag.getEvent(&mag_event);
 
-  if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation))
+  /* Calculate pitch and roll from the raw accelerometer data */
+  accel.getEvent(&accel_event);
+  if (dof.accelGetOrientation(&accel_event, &orientation))
   {
-    gyroscopeData[0] = orientation.roll;
-    gyroscopeData[1] = orientation.pitch;
-    gyroscopeData[2] = orientation.heading;
+    /* 'orientation' should have valid .roll and .pitch fields */
+    Serial.print(F("Roll: "));
+    Serial.print(orientation.roll);
+    Serial.print(F("; "));
+    Serial.print(F("Pitch: "));
+    Serial.print(orientation.pitch);
+    Serial.print(F("; "));
   }
-  return gyroscopeData;
+  
+  /* Calculate the heading using the magnetometer */
+  mag.getEvent(&mag_event);
+  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
+  {
+    Serial.print(F("Heading: "));
+    Serial.print(orientation.heading);
+    Serial.print(F("; "));
+  }
+
+  Serial.println(F(""));
+
+  /********RFID chip********/
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+    Serial.println(F("A new card has been detected."));
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+
+    Serial.println();
+    Serial.print(F("In dec: "));
+    printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+  }
+  else Serial.println(F("Card read previously."));
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
 }
 
-/**
- * get acceleration sensor values  
- * @return {float*} - acceleration array values [x,y,z]
- */
-float* getAccelerationData(sensors_event_t accel_event, float *accelerationData){
-  accel.getEvent(&accel_event);
-  accelerationData[0] = accel_event.acceleration.x;
-  accelerationData[1] = accel_event.acceleration.y;
-  accelerationData[2] = accel_event.acceleration.z;
-  return accelerationData;
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i], DEC);
+  }
 }
