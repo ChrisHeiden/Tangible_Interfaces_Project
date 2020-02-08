07051132ad39e9ea94d441f5d57ad7cb3f7e4172
ChrisHeiden <christopherheiden9@gmail.com>
2020-02-08 10:38:09 +0100

    update Interaction sender to send information over WIFI

--- InteractionSender/InteractionSender.ino
+++ InteractionSender/InteractionSender.ino
@@ -1,5 +1,10 @@
 #include <Wire.h>
 
+/********WIFI********/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
 /********Distance Sensor********/
 #include <VL53L1X.h>
 
@@ -11,13 +16,15 @@
 #include <Adafruit_L3GD20_U.h>
 #include <Adafruit_9DOF.h>
 
-/********RFID chip********/
+/********RFID Sensor********/
 #include <SPI.h>
 #include <MFRC522.h>
 
 #define SS_PIN D8
 #define RST_PIN D3
- 
+
+const char* ssid = "TangibleInteractionProject";
+ESP8266WebServer server(80); 
 
 VL53L1X distanceSensor;
 
@@ -31,19 +38,22 @@ MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 MFRC522::MIFARE_Key key; 
 byte nuidPICC[4];
 
-void setup()
-{
-  Serial.begin(115200);
+void setup(){
+  delay(200);
+  Serial.begin(115200);      
+  
+  /********WIFI********/
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();     
+  server.on("/", handleRoot);           
+  server.begin(); 
+  
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
 
   /********Distance Sensor********/
   distanceSensor.setTimeout(500);
-  if (!distanceSensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1);
-  }
+  if (!distanceSensor.init()) { while (1); }
   
   distanceSensor.setDistanceMode(VL53L1X::Long);
   distanceSensor.setMeasurementTimingBudget(50000);
@@ -51,130 +61,134 @@ void setup()
 
   /********Accel Sensor********/
   #ifndef ESP8266
-    while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+    while (!Serial);
   #endif
-    Serial.begin(115200);
-    Serial.println("Accelerometer Test"); Serial.println("");
-
-    /* Initialise the sensor */
-    if(!accel.begin())
-    {
-      /* There was a problem detecting the ADXL345 ... check your connections */
-      Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-      while(1);
-    }
+    if(!accel.begin()) { while(1); }
 
   /********Orientation Sensor********/
-  if(!accel.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
-    while(1);
-  }
-  if(!mag.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-    while(1);
-  }
-
-  /********RFID chip********/
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
-
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
-
-  Serial.println(F("This code scan the MIFARE Classsic NUID."));
-  Serial.print(F("Using the following key:"));
+  if(!accel.begin()) { while(1); }
+  if(!mag.begin()) { while(1); }
+  
+ /********RFID Sensor********/
+  SPI.begin();
+  rfid.PCD_Init();
+  
+  for (byte i = 0; i < 6; i++) { key.keyByte[i] = 0xFF; }
 }
 
-void loop()
-{
-  /********Distance Sensor********/
-  Serial.print(distanceSensor.read()); // in mm
-  if (distanceSensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+void loop(){
+  server.handleClient();
+}
 
-  Serial.println();
+/**
+ * send music data to client
+ */
+void handleRoot() {
+  sensors_event_t accel_event;
+  
+  /********Distance Sensor********/
+  int distance = getDistance();
 
   /********Accel Sensor********/
-  sensors_event_t accel_event;
-  accel.getEvent(&accel_event);
-
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: "); Serial.print(accel_event.acceleration.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(accel_event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(accel_event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+   float accelerationData[3] = {1,2,3};
+   float* accelerationDataPointer = getAcceleration(accel_event, accelerationData);
 
   /********Orientation Sensor********/
+  float orientationData[3] = {1,2,3};
+  float* orientationPointer = getOrientation(accel_event, orientationData);
+
+  /********RFID Sensor********/
+  int tag = getRFIDTag();
+
+  String rfidString = String(tag);
+  String distString = String(distance);
+  String oriXString = String(orientationPointer[0]);
+  String oriYString = String(orientationPointer[1]);
+  String oriZString = String(orientationPointer[2]);
+  String accXString = String(accelerationDataPointer[0]);
+  String accYString = String(accelerationDataPointer[1]);
+  String accZString = String(accelerationDataPointer[2]);
+  
+  String s = "Music Data: RFID[" + rfidString + "] Distance[" + distString + "], Orientation[" + oriXString + "," + oriYString + ","  + oriZString + "], Acceleration[" + accXString + "," + accYString + ","  + accZString + "]";  
+  //String s = "Music Data: RFID[312] Distance[21], Orientation[14,50,10], Acceleration[1,1,1]";  
+
+  server.send(200,"text/plain",s);      
+}
+
+/**
+ * get tag ID 
+ * @return {int} - tag ID
+ */
+int getRFIDTag(){
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return -1;
+
+  if ( ! rfid.PICC_ReadCardSerial())
+    return -1;
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+      rfid.uid.uidByte[1] != nuidPICC[1] || 
+      rfid.uid.uidByte[2] != nuidPICC[2] || 
+      rfid.uid.uidByte[3] != nuidPICC[3] ) {
+      int tag = -1;
+
+      for (byte i = 0; i < 4; i++) {
+        nuidPICC[i] = rfid.uid.uidByte[i];
+        tag += rfid.uid.uidByte[i];
+      }
+  
+      // Halt PICC
+      rfid.PICC_HaltA();
+
+      // Stop encryption on PCD
+      rfid.PCD_StopCrypto1();
+      return tag;
+  }
+}
+
+/**
+ * get orientation sensor values  
+ * @return {float*} - orientation array values [roll,pitch,heading]
+ */
+float* getOrientation(sensors_event_t accel_event, float *orientationData){
   sensors_event_t mag_event;
   sensors_vec_t orientation;
 
-  /* Calculate pitch and roll from the raw accelerometer data */
   accel.getEvent(&accel_event);
   if (dof.accelGetOrientation(&accel_event, &orientation))
   {
-    /* 'orientation' should have valid .roll and .pitch fields */
-    Serial.print(F("Roll: "));
-    Serial.print(orientation.roll);
-    Serial.print(F("; "));
-    Serial.print(F("Pitch: "));
-    Serial.print(orientation.pitch);
-    Serial.print(F("; "));
+    orientationData[0] = orientation.roll;
+    orientationData[1] = orientation.pitch;
   }
   
-  /* Calculate the heading using the magnetometer */
   mag.getEvent(&mag_event);
   if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
   {
-    Serial.print(F("Heading: "));
-    Serial.print(orientation.heading);
-    Serial.print(F("; "));
+    orientationData[2] = orientation.heading;
   }
 
-  Serial.println(F(""));
-
-  /********RFID chip********/
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! rfid.PICC_IsNewCardPresent())
-    return;
-
-  // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
-    return;
-
-  Serial.print(F("PICC type: "));
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-  Serial.println(rfid.PICC_GetTypeName(piccType));
-
-  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-    rfid.uid.uidByte[1] != nuidPICC[1] || 
-    rfid.uid.uidByte[2] != nuidPICC[2] || 
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
-    Serial.println(F("A new card has been detected."));
-
-    // Store NUID into nuidPICC array
-    for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = rfid.uid.uidByte[i];
-    }
-
-    Serial.println();
-    Serial.print(F("In dec: "));
-    printDec(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-  }
-  else Serial.println(F("Card read previously."));
-
-  // Halt PICC
-  rfid.PICC_HaltA();
-
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
+  return orientationData;
 }
 
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i], DEC);
-  }
+/**
+ * get acceleration sensor values  
+ * @return {float*} - acceleration array values [x,y,z]
+ */
+float* getAcceleration(sensors_event_t accel_event, float *accelerationData){
+  accel.getEvent(&accel_event);
+  accelerationData[0] = accel_event.acceleration.x;
+  accelerationData[1] = accel_event.acceleration.y;
+  accelerationData[2] = accel_event.acceleration.z;
+  return accelerationData;
+}
+
+/**
+ * get distance sensor value  
+ * @return {int} - distance
+ */
+int getDistance(){
+  int distance;
+  distance = distanceSensor.read(); // in mm
+  if (distanceSensor.timeoutOccurred()) { distance = -1; }
+  return distance;
 }
